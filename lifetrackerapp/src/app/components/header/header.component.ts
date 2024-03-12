392485e4c208c95873ff2b25d354c7cafff46022
Perry Chambers <perrychambers97@gmail.com>
2024-03-12 00:19:16 -0400

    Refactored sidenav code to not included the router outlet in order to decouple main view from sidenav component.

--- lifetrackerapp/src/app/components/header/header.component.ts
+++ lifetrackerapp/src/app/components/header/header.component.ts
@@ -4,13 +4,14 @@ import { MatIconModule } from '@angular/material/icon'
 import { RouterModule } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidenavComponent } from "../sidenav/sidenav.component";
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [MatToolbarModule, MatIconModule, RouterModule, MatSidenavModule],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+    selector: 'app-header',
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css',
+    imports: [MatToolbarModule, MatIconModule, RouterModule, MatSidenavModule, SidenavComponent]
 })
 export class HeaderComponent {
   @Output() onToggleSidenav: EventEmitter<any> = new EventEmitter<any>();
